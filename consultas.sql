3f899f211c41d6b914f5b090e57b054756b25b04
BrunoNasc74 <bruno.hpn74@gmail.com>
2022-07-10 13:52:46 -0300

    perguntas adicionadas

--- consultas.sql
+++ consultas.sql
@@ -10,3 +10,58 @@ got_episodes_v4.Title,
 got_episodes_v4.Director
 FROM got_episodes_v4
 WHERE Director IN ('Michael Slovis');
+
+-- Todos os episódios com a nota acima da média
+SELECT 
+	Season  `Temporada`,
+    Episode `Episódio`,
+    Title   `Título`,
+    Rating  `Nota`,
+    Summary `Resumo`
+FROM got_episodes_v4
+    WHERE Rating > (
+        SELECT AVG(Rating) 
+        FROM got_episodes_v4
+        )
+ORDER BY Rating DESC;
+
+-- Todos episódios com a nota abaixo da média
+
+SELECT 
+	Season  `Temporada`,
+    Episode `Episódio`,
+    Title   `Título`,
+    Rating  `Nota`,
+    Summary `Resumo`
+FROM got_episodes_v4
+    WHERE Rating < (
+        SELECT AVG(Rating) 
+        FROM got_episodes_v4
+        )
+ORDER BY Rating ASC;
+
+-- Personagens principais da série e o número de aparições em cada episódio
+
+SELECT 
+	characters_v4.Character `Personagens`,
+	COUNT(Star_1) `N.º de aparição como personagem principal em cada episódio`
+FROM characters_v4
+    INNER JOIN got_episodes_v4
+        ON `Actor/ess` = Star_1
+        GROUP BY Star_1
+ORDER BY COUNT(Star_1) DESC;
+
+-- Média da nota por temporada
+
+SELECT 
+	Season `Temporada`,
+    ROUND(AVG(Rating), 1) `Média da nota`
+FROM got_episodes_v4
+    GROUP BY Season
+ORDER BY ROUND(AVG(Rating), 1) DESC;
+
+-- Duração total da série
+
+SELECT 
+    SEC_TO_TIME(SUM(Duration*60)) `Duração total da série`
+FROM got_episodes_v4;
